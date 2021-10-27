2e938e9c096649b111bf6fa1deced418766029ff
jwklong <72522395+jwklong@users.noreply.github.com>
2021-10-27 11:08:32 +0100

    v0.2.1-dev1
    another update wooo

--- Codename-Delta.lua
+++ Codename-Delta.lua
@@ -3,47 +3,10 @@
 getgenv().tips = true
 
 local Prefix = ":"
-local BotVersion = "Codename Delta - v0.2.0b"
+local BotVersion = "Codename Delta - v0.2.1-dev1"
 local Blacklist = {}
 local Players = {}
 local LPlr = game:GetService("Players").LocalPlayer
-local insults = {
-	"Anne Frank had a better living state than you",
-	"You look like a barbie doll which i gave a makeover with my markers",
-	"Your momma back built like a skate ramp I can do an ollie off her",
-	"You look like the 1% of germs that hand-soap doesn't kill",
-	"Boy u literally be smelling like tv oil",
-	"No wonder your parents hate you",
-	"You haven't had any neurological brain development in the last decade",
-	"I will build a sandcastle out of your dead nans ashes",
-	"Your brain has the capacity of a dried sea sponge",
-	"You make me cringe, I literally want to quit this game because of you",
-	"Your outfit is horrendous",
-	"Your name is horrendous",
-	"Your IQ is so low that even scientists cant zoom in on it",
-	"You are a object",
-	"You are fat",
-	"You stink of your nans cremation",
-	"How many people does it take to screw in a lightbulb? 20 of your kind",
-	"I can smell you through the screen",
-	"Candy shop got robbed, didn't know you did it",
-	"Your brain is the size of an average ants",
-	"Everything you've worked for, your whole life is embarassing",
-	"Go back to school",
-	"Are you dumb or just young",
-	"You make me feel e-sick",
-	"I very much dislike you",
-	"Settle along you peasent",
-	"You are worth nothing",
-	"I hope you suffer",
-	"Your words, they make no sense",
-	"Please leave the server you cretin",
-	"You make me sick",
-	"Huh? Didn't know infants could play roblox",
-	"I bet you live in a poverty stricken country",
-	"You belong in the special ed class",
-}
-
 
 function Lchat(msg) --local chat
 	game.StarterGui:SetCore("ChatMakeSystemMessage", {Text = msg;})	
@@ -51,7 +14,6 @@ end
 function Chat(msg)
 	game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(msg,"All")
 end
-local animdisabled = false
 local on = true
 
 local function GetTip(tip)
@@ -80,13 +42,13 @@ local function Chatted(msg,plr)
 	if string.sub(msg,1,1) == Prefix and on == true and not table.find(Blacklist,plr.Name) then
 		if string.lower(string.sub(msg,2,5)) == "help" then
 			-- to add more pages add extra elseifs
-			-- string.sub(msg, 7, #msg) == "page number here" should work
+			-- string.sub(msg, 7, #msg) == "page number/name here" should work
 			if string.sub(msg, 7, #msg) == "1"  or string.sub(msg, 6, #msg) == "" then
 				Chat("Prefix: "..Prefix.."  Page: 1  Commands: help (page), about, source, version, pages, jump, trip, prefix (new), say (text), goto (plr)")
 			elseif string.sub(msg, 7, #msg) == "2" then
 				Chat("Prefix: "..Prefix.."  Page: 2  Commands: bringbot")
 			elseif string.sub(msg, 7, #msg) == "bot-only" then
-				Chat("Prefix: "..Prefix.."  Page: bot-only  Commands: stop, blacklist (plr), unblacklist (plr)")
+				Chat("Prefix: "..Prefix.."  Page: bot-only  Commands: stop, blacklist (plr), unblacklist (plr), reset, invincible")
 			elseif string.sub(msg, 7, #msg) == "testing" then
 				Chat("Prefix: "..Prefix.."  Page: testing  Commands: tip (num)")
 			else
@@ -96,19 +58,18 @@ local function Chatted(msg,plr)
 			Chat("Version: "..BotVersion)
 		elseif string.lower(string.sub(msg,2,7)) == "source" then
 			Chat("The source is available on GitHub, just search Codename-Delta on it and click on the lua repository!")
-			Chat("For users with exploits the source has been copied to your clipboard.")
-			if setclipboard() then 
-				setclipboard(game:HttpGet("https://raw.githubusercontent.com/jwklong/Codename-Delta/main/Codename-Delta.lua"))
-			end
 		elseif string.lower(string.sub(msg,2,7)) == "pages" then
 			Chat("Pages: 1-2. Special Pages: bot-only, testing. How to use: Do :help (page)")
 		elseif string.lower(string.sub(msg,2,6)) == "about" then
 			Chat("Codename Delta is a advanced bot that can respond at instantaneous speeds (if ping isn't very high) and do complex pathfinding calculations!")
 		elseif string.lower(string.sub(msg,2,5)) == "jump" then
-			if LPlr.Character.Humanoid.JumpPower < 50 then
+			local oldjumppower = LPlr.Character.Humanoid.JumpPower * 0.5 * 2 --save old jump power
+			if oldjumppower < 50 then
 				LPlr.Character.Humanoid.JumpPower = 50
 			end
 			LPlr.Character.Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+			wait()
+			LPlr.Character.Humanoid.JumpPower = oldjumppower
 		elseif string.lower(string.sub(msg,2,7)) == "prefix" then
 			if string.len(string.lower(string.sub(msg,9,#msg))) > 1 then
 				Chat("ERROR: Invalid Prefix.")
@@ -122,16 +83,6 @@ local function Chatted(msg,plr)
 			Chat(string.sub(msg,6,#msg).." - Said by "..plr.Name)
 		elseif string.lower(string.sub(msg,2,5)) == "trip" then
 			LPlr.Character.Humanoid.Sit = true
-		elseif string.lower(string.sub(msg,2,5)) == "limp" then
-			for i,v in pairs(LPlr.Character:GetChildren()) do
-				if v:IsA("Part") then
-					if v.Name ~= ("Head" or "Torso" or "UpperTorso")
-						v:Destroy();	
-					end
-				end
-			end
-		elseif string.lower(string.sub(msg,2,5)) == "noanim" then
-			pcall(function()LPlr.Character.Animate.Disabled = ~animdisabled end)
 		elseif string.lower(string.sub(msg,2,5)) == "goto" then
 			if game.Players:FindFirstChild(string.sub(msg,7,#msg)) then
 				if LPlr.Character.Humanoid.JumpPower < 50 then
@@ -200,13 +151,19 @@ local function Chatted(msg,plr)
 					Chat("Player is not blacklisted")
 				end
 			end
+		elseif string.lower(string.sub(msg,2,6)) == "reset" then
+			if plr.Name == LPlr.Name then
+				LPlr.Character.Humanoid.Health = 0
+			end
+		elseif string.lower(string.sub(msg,2,11)) == "invincible" then --NOTE: this is not invincible to things that set health to 0, only ones like weapons. e.g. a sword
+			if plr.Name == LPlr.Name then
+				LPlr.Character.Humanoid.MaxHealth = 2^1023
+				LPlr.Character.Humanoid.Health = 2^1023
+			end
 		elseif string.lower(string.sub(msg,2,4)) == "tip" then
 			GetTip(tonumber(string.sub(msg,6,#msg)))
 		elseif string.lower(string.sub(msg,2,9)) == "bringbot" then
 			LPlr.Character:SetPrimaryPartCFrame(plr.Character.HumanoidRootPart.CFrame)
-		elseif string.lower(string.sub(msg,2,9)) == "roast" then
-			local person = string.split(msg," ")[2]
-			Chat("Dear " .. person .. insults[math.random(1,#insults)])
 		end	
 	end	
 end
